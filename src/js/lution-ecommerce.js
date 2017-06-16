705434af6ab7ac5b9a99eb2589c673bdd5449d7d
Juan Gallo <juani.reicko@gmail.com>
2017-06-16 00:48:58 -0300

    Click handler and first ajax call

--- src/js/lution-ecommerce.js
+++ src/js/lution-ecommerce.js
@@ -21,7 +21,8 @@
     lution.container = container;
 
     lution.params = params;
-    lution.hasJquery = typeof window.jQuery != 'undefined';
+    // lution.hasJquery = typeof window.jQuery != 'undefined';
+    lution.hasJquery = false;
 
     if (container.slice(0, 1) == '#') {
       lution.containerType = 'id';
@@ -32,26 +33,98 @@
       return;
     }
 
-    if (!lution.hasJquery) {
-      lution.container = lution.container.slice(1, lution.container.length);
+    if (lution.hasJquery == false) {
+      container = container.slice(1, container.length);
     }
 
-    lution.addClass = function(container, className) {
+    if (lution.hasJquery) {
+      lution.container = $(container);
+    } else {
+      if (lution.containerType == 'class') {
+        lution.container = document.getElementsByClassName(container)[0];
+      } else {
+        lution.container = document.getElementById(container);
+      }
+    }
+
+    lution.addClass = function(element, className) {
       if (lution.hasJquery) {
-        $(container).addClass(className);
+        element.addClass(className);
       } else {
         if (lution.containerType == 'class') {
-          document.getElementsByClassName(container)[0].className += " " + className;
+          element.className += " " + className;
         } else {
-          document.getElementById(container).className += " " + className;
+          element.className += " " + className;
         }
       }
 
       return container;
     };
 
+    lution.initBot = function(e) {
+      console.log(1234);
+      e = e || window.event;
+      var target = e.target || e.srcElement;
+      target.removeEventListener('click', lution.initBot, false);
 
-    console.log(123);
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+         var data = JSON.parse(this.responseText);
+        } else {
+          console.log('AJAX error');
+        }
+      };
+      xhttp.open("GET", "https://www.lutionbot.com/api/company/tan%20intensa?startChat=true", true);
+      xhttp.send();
+      // $.ajax({
+      //   type: "GET",
+      //   url: 'https://www.lutionbot.com/api/company/tan%20intensa?startChat=true',
+      //   success: function(data) {
+      //     $("#chat-input").keypress(function(e) {
+      //       if(e.which == 13) {
+      //         chooseSearchType();
+      //       }
+      //     });
+      //     initCompany(data);
+      //     openChat();
+      //     $(".chat-title").text("LutionBot");
+      //     $(".chat-title").attr("onclick","closeChat()");
+      //     insertBotMessage(companyFirstGreeting());
+      //   },
+      //   error: function(err) {
+      //     insertBotMessage("Disculpe, estamos sufriendo algunas dificultades técnicas. Intente realizar su consulta más tarde.");
+      //   }
+      // });
+    };
+
+
+
+
+    lution.createDiv = function(innerText, parent, options) {
+      var container = document.createElement("div"),
+        node = document.createTextNode(innerText);
+
+      if (options.className) {
+        container.className = options.className;
+      }
+
+      if (options.clickFunction) {
+        if (container.addEventListener) {
+          container.addEventListener("click", options.clickFunction, false);
+        } else {
+          if (container.attachEvent) {
+            container.attachEvent("click", options.clickFunction);
+          }
+        }
+      }
+
+      container.appendChild(node);
+      parent.appendChild(container);
+    };
+
+    lution.createDiv('Hola amigos', lution.container, {className: 'chat-title', clickFunction: lution.initBot});
+
     lution.container = lution.addClass(lution.container, 'prueba');
 
   };
