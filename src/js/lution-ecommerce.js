872d98a4eaf404109dfe051e35fcdf4cff439674
Juan Gallo <juani.reicko@gmail.com>
2017-06-16 01:11:49 -0300

    Add some style, open chat and change title

--- src/js/lution-ecommerce.js
+++ src/js/lution-ecommerce.js
@@ -71,16 +71,23 @@
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
          var data = JSON.parse(this.responseText);
-        } else {
-          console.log('AJAX error');
+          console.log(data);
+          document.getElementById('chat-input').onkeypress = function(e) {
+            if (e.which == 13) {
+
+            }
+          };
+          document.getElementsByClassName('chat-window')[0].classList.remove('dont-show');
+          document.getElementsByClassName('chat-title')[0].addEventListener("click", lution.closeChat, false);
+          document.getElementsByClassName('chat-title')[0].innerHTML = "LutionBot";
+
+          // $(".chat-title").attr("onclick","closeChat()");
         }
       };
       xhttp.open("GET", "https://www.lutionbot.com/api/company/tan%20intensa?startChat=true", true);
       xhttp.send();
-      // $.ajax({
-      //   type: "GET",
-      //   url: 'https://www.lutionbot.com/api/company/tan%20intensa?startChat=true',
-      //   success: function(data) {
+
+
       //     $("#chat-input").keypress(function(e) {
       //       if(e.which == 13) {
       //         chooseSearchType();
@@ -89,21 +96,17 @@
       //     initCompany(data);
       //     openChat();
       //     $(".chat-title").text("LutionBot");
-      //     $(".chat-title").attr("onclick","closeChat()");
       //     insertBotMessage(companyFirstGreeting());
-      //   },
-      //   error: function(err) {
-      //     insertBotMessage("Disculpe, estamos sufriendo algunas dificultades técnicas. Intente realizar su consulta más tarde.");
-      //   }
-      // });
+
     };
 
+    lution.closeChat = function() {
 
+    }
 
 
     lution.createDiv = function(innerText, parent, options) {
-      var container = document.createElement("div"),
-        node = document.createTextNode(innerText);
+      var container = document.createElement("div");
 
       if (options.className) {
         container.className = options.className;
@@ -119,17 +122,41 @@
         }
       }
 
-      container.appendChild(node);
+      if (innerText != false) {
+        var node = document.createTextNode(innerText);
+        container.appendChild(node);
+      }
+
+      parent.appendChild(container);
+    };
+
+    lution.createInput = function(type, parent, options) {
+      var container = document.createElement("INPUT");
+      container.setAttribute("type", type);
+
+      if (options.className) {
+        container.setAttribute("class", options.className);
+      }
+
+      if (options.id) {
+        container.setAttribute("id", options.id);
+      }
+
       parent.appendChild(container);
     };
 
     lution.createDiv('Hola amigos', lution.container, {className: 'chat-title', clickFunction: lution.initBot});
+    lution.createDiv(false, lution.container, {className: 'chat-window dont-show'});
+    lution.createDiv(false, document.getElementsByClassName('chat-window')[0], {className: 'chat-box'});
+    lution.createDiv(false, document.getElementsByClassName('chat-window')[0], {className: 'chat-clearBoth'});
+    lution.createDiv(false, document.getElementsByClassName('chat-window')[0], {className: 'chat-input-box'});
+    lution.createInput('text', document.getElementsByClassName('chat-input-box')[0], {className: 'chat-input', id: 'chat-input'});
 
-    lution.container = lution.addClass(lution.container, 'prueba');
+    // lution.container = lution.addClass(lution.container, 'prueba');
 
   };
 
   window.Lution = Lution;
 })();
 
-var lution = new Lution('.test-container');
+var lution = new Lution('.chat-container');
